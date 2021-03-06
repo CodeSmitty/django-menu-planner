6e148360a38eee7e0baa3550c12f0aed56ea94b9
nfoos <nfoos@rm-f.me>
2021-03-06 16:10:25 -0500

    Add Meal and MealItem models

--- menu_planner/meals/models.py
+++ menu_planner/meals/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Meal(models.Model):
+    date = models.DateField(db_index=True)
+    type = models.CharField(
+        choices = [
+            ('lunch', 'Lunch'),
+            ('dinner', 'Dinner'),
+        ],
+        max_length = 10
+    )
+
+    class Meta:
+        unique_together = ['date', 'type']
+        ordering = ['date', '-type']
+
+class MealItem(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(
+        choices = [
+            ('entre', 'Entre'),
+            ('side', 'Side'),
+            ('other', 'Other'),
+        ],
+        max_length = 10
+    )
+    diets = models.JSONField()
